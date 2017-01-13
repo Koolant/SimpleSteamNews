216491a73f3fd9dfbe5643dd5fafa8fbc5d2b8e1
Ryan <Ryan@Home>
2017-01-13 15:14:01 -0800

    Updated input in the console.

--- SteamInterface/Program.cs
+++ SteamInterface/Program.cs
@@ -25,23 +25,36 @@ namespace SteamInterface
             AppListRoot applistRoot = new AppListRoot();
             applistRoot = JsonConvert.DeserializeObject<AppListRoot>(jsonString);   // Deserializes the JSON data into objects containing two properties: AppID and Name.
 
-            int targetObjId = (from t in applistRoot.applist.apps          // This lambda expression will search through the objects for the first one containing the users search string and return the property called "appid" of that object.
-                               where t.name.Contains("User Input Here")    // The reason for this is that the news articles are found in a different set of JSON data that is indexed by appid rather than title.
-                               select t).First().appid;                    // Thus the appid that corresponds to the title must be retrieved first in order to query the JSON data that contains the news.
-
-            string getNewUrlString = steamMethodLibrary.newsFeedConcatenator(targetObjId);  // This string is the new http request built from the AppId we've just located.
-
-            string newJsonString = myRetriever.RetrieveJson(getNewUrlString); // This string contains the result of the new http request.
-
-            AppNewsRoot appNewsRoot = new AppNewsRoot();
-            appNewsRoot = JsonConvert.DeserializeObject<AppNewsRoot>(newJsonString); // This deserializes the news feed JSON data into an object.
-
-            string articleText = appNewsRoot.appnews.newsItems.First().contents ;    // This pulls the body of the article from the "contents" property of the object.
-                                                                                     // The article body comes down as html so I will need to format this once I have a functional UI.
+            bool endProgram = false;
             
-            Console.WriteLine(articleText);
-            Console.ReadKey();
-          
+            while (!endProgram)
+            {
+                string userInput = Console.ReadLine();
+
+                int targetObjId = (from t in applistRoot.applist.apps          // This lambda expression will search through the objects for the first one containing the users search string and return the property called "appid" of that object.
+                                   where t.name.Contains(userInput)    // The reason for this is that the news articles are found in a different set of JSON data that is indexed by appid rather than title.
+                                   select t).First().appid;                    // Thus the appid that corresponds to the title must be retrieved first in order to query the JSON data that contains the news.
+
+                string getNewUrlString = steamMethodLibrary.newsFeedConcatenator(targetObjId);  // This string is the new http request built from the AppId we've just located.
+
+                string newJsonString = myRetriever.RetrieveJson(getNewUrlString); // This string contains the result of the new http request.
+
+                AppNewsRoot appNewsRoot = new AppNewsRoot();
+
+                appNewsRoot = JsonConvert.DeserializeObject<AppNewsRoot>(newJsonString); // This deserializes the news feed JSON data into an object.
+
+                string articleText = appNewsRoot.appnews.newsItems.First().contents;    // This pulls the body of the article from the "contents" property of the object.
+                                                                                        // The article body comes down as html so I will need to format this once I have a functional UI.
+
+                Console.WriteLine(articleText);
+                Console.WriteLine("\r\nEnd? y/n");
+
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    endProgram = true;
+                }
+                Console.Clear();
+            }
         }
     }
 }
