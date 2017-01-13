773b8ab047b1477ed09cc85f7039758c50077f29
ryan <ryan@DESKTOP-3CODL85>
2017-01-13 03:34:57 -0800

    Add project files.

---
+++ SteamInterface/ClassLib.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
+
+namespace SteamInterface
+{
+    class AppListRoot //These Classes are the framework for parsing the app id list from the web api
+    {
+        public Applist applist { get; set; }
+    }
+    class Applist
+    {
+        public List<Apps> apps { get; set; }
+    }
+    class Apps
+    {
+        public int appid { get; set; }
+
+        public string name { get; set; }
+    }
+
+    class AppNewsRoot
+    {
+        public AppNews appnews { get; set; }
+    }
+    class AppNews
+    {
+        public int appid { get; set; }
+        public List<NewsItems> newsItems { get; set; }
+    }
+    class NewsItems
+    {
+        public string gid { get; set; }
+
+        public string title { get; set; }
+
+        public string url { get; set; }
+
+        public bool is_external_url { get; set; }
+
+        public string author { get; set; }
+
+        public string contents { get; set; }
+
+        public string feedlabel { get; set; }
+
+        public int date { get; set; }
+
+        public string feedname { get; set; }
+    }
+
+    class SteamMethodUrlLibrary // This class builds strings to be fed into the Retriever class
+    {
+        private string baseString = "http://api.steampowered.com";
+
+        public string appNamesIds
+        {
+            get
+            {
+                return baseString + "/ISteamApps/GetAppList/v0002/";
+            }
+        }
+
+        public string newsFeedConcatenator(int myAppid)     // This function takes the AppId as an argument and concatenates it into an http request for the most recent news entry corresponding to that ID
+        {
+                return baseString + "/ISteamNews/GetNewsForApp/V0002/?format=json&appid=" + myAppid + "&count=1";
+        }
+    }
+
+    class Retriever
+    {
+        private WebClient web = new WebClient();
+
+        public string RetrieveJson(string uriString) // This function takes a string as an argument, converts it into a URI and attempts a web request.
+        {            
+            Uri appendedUri = new Uri(uriString);
+
+            try
+            {
+                return web.DownloadString(appendedUri);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+                return null;
+            }
+        }
+    }
+    /*abstract class QueryBuilder
+    {
+        public QueryBuilder()
+        {
+
+        }
+    }
+    */
+
+}
---
+++ SteamInterface/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace SteamInterface
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retriever myRetriever = new Retriever();
+            SteamMethodUrlLibrary steamMethodLibrary = new SteamMethodUrlLibrary();
+
+            string appNamesIdsUrlString = steamMethodLibrary.appNamesIds;   // This string contains the hard coded URL of the JSON data containing the Names and IDs of all available products.
+
+            string jsonString = myRetriever.RetrieveJson(appNamesIdsUrlString); // This string contains the JSON data pulled from the Steam API.
+
+            AppListRoot applistRoot = new AppListRoot();
+            applistRoot = JsonConvert.DeserializeObject<AppListRoot>(jsonString);   // Deserializes the JSON data into objects containing two properties: AppID and Name.
+
+            int targetObjId = (from t in applistRoot.applist.apps          // This lambda expression will search through the objects for the first one containing the users search string and return the property called "appid" of that object.
+                               where t.name.Contains("User Input Here")    // The reason for this is that the news articles are found in a different set of JSON data that is indexed by appid rather than title.
+                               select t).First().appid;                    // Thus the appid that corresponds to the title must be retrieved first in order to query the JSON data that contains the news.
+
+            string getNewUrlString = steamMethodLibrary.newsFeedConcatenator(targetObjId);  // This string is the new http request built from the AppId we've just located.
+
+            string newJsonString = myRetriever.RetrieveJson(getNewUrlString); // This string contains the result of the new http request.
+
+            AppNewsRoot appNewsRoot = new AppNewsRoot();
+            appNewsRoot = JsonConvert.DeserializeObject<AppNewsRoot>(newJsonString); // This deserializes the news feed JSON data into an object.
+
+            string articleText = appNewsRoot.appnews.newsItems.First().contents ;    // This pulls the body of the article from the "contents" property of the object.
+                                                                                     // The article body comes down as html so I will need to format this once I have a functional UI.
+            
+            Console.WriteLine(articleText);
+            Console.ReadKey();
+          
+        }
+    }
+}
+ 
